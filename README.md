b658fdf79aa2854004a78274963707a11a3d1a76
Nilton Sousa <niltonmiguel0@gmail.com>
2026-04-09 16:54:02 -0300

    iniciando com animação

---
+++ README.md
@@ -0,0 +1,3 @@
+# animacao_flutter
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool clicou = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                clicou = !clicou;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+              width: double.infinity,
+              height: double.infinity,
+              alignment: clicou ? Alignment.topRight : Alignment.bottomLeft,
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: clicou ? Colors.orange : Colors.blue,
+                  borderRadius: clicou
+                      ? BorderRadius.circular(50)
+                      : BorderRadius.circular(0),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
